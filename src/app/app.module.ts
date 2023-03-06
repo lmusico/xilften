2825311e8588037e29d10657bc7e283a3610752f
Lucas Músico <lucasmusico23@hotmail.com>
2023-03-06 11:45:36 -0300

    Limpeza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
